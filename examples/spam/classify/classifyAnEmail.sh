fed3b91b891831ee12d2636dfd49c876fc18643a
m3o-actions <>
2021-11-11 23:36:17 +0000

    Commit from m3o/m3o action

--- examples/spam/classify/classifyAnEmail.sh
+++ examples/spam/classify/classifyAnEmail.sh
@@ -2,8 +2,8 @@ curl "https://api.m3o.com/v1/spam/Classify" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
-  "email_body": "Hi there,\n\nWelcome to M3O.\n\nThanks\nM3O team",
   "from": "noreply@m3o.com",
   "subject": "Welcome",
+  "text_body": "Hi there,\n\nWelcome to M3O.\n\nThanks\nM3O team",
   "to": "hello@example.com"
 }'
