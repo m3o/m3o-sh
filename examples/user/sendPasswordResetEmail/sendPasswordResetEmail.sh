94ee9d89251e066624e3e4efc8cf8bb64beb531c
m3o-actions <>
2021-11-16 14:48:58 +0000

    Commit from m3o/m3o action

---
+++ examples/user/sendPasswordResetEmail/sendPasswordResetEmail.sh
@@ -0,0 +1,9 @@
+curl "https://api.m3o.com/v1/user/SendPasswordResetEmail" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "email": "joe@example.com",
+  "fromName": "Awesome Dot Com",
+  "subject": "Password reset",
+  "textContent": "Hi there,\n click here to reset your password: myapp.com/reset/code?=$code"
+}'
