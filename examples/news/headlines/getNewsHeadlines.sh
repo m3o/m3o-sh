c64230ead822a7db35d3ae907749bcfa47d23c96
m3o-actions <>
2021-11-24 10:46:55 +0000

    Commit from m3o/m3o action

---
+++ examples/news/headlines/getNewsHeadlines.sh
@@ -0,0 +1,8 @@
+curl "https://api.m3o.com/v1/news/Headlines" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "date": "2021-11-24",
+  "language": "en",
+  "locale": "us"
+}'
