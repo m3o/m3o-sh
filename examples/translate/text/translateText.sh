5475036900c6fd4d93d5eed88b62b8a2d6a1116b
m3o-actions <>
2021-12-10 11:19:16 +0000

    Commit from m3o/m3o action

---
+++ examples/translate/text/translateText.sh
@@ -0,0 +1,10 @@
+curl "https://api.m3o.com/v1/translate/Text" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "content": "hello",
+  "format": "text",
+  "model": "nmt",
+  "source": "en",
+  "target": "fr"
+}'
