461f509e5f11ee6ca6a886754f13a32763c1f7ff
m3o-actions <>
2021-12-10 11:37:51 +0000

    Commit from m3o/m3o action

---
+++ examples/youtube/embed/embedAYoutubeVideo.sh
@@ -0,0 +1,6 @@
+curl "https://api.m3o.com/v1/youtube/Embed" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "url": "https://www.youtube.com/watch?v=GWRWZu7XsJ0"
+}'
