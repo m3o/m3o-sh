e942a4cdc203678a40bbcc9c6d802e5426b4823d
m3o-actions <>
2021-12-11 08:24:50 +0000

    Commit from m3o/m3o action

---
+++ examples/avatar/generate/generateAnAvatarAndUploadTheAvatarToMicrosCdn.sh
@@ -0,0 +1,9 @@
+curl "https://api.m3o.com/v1/avatar/Generate" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "format": "jpeg",
+  "gender": "female",
+  "upload": false,
+  "username": ""
+}'
---
+++ examples/avatar/generate/generateAvatarAndReturnBase64stringOfTheAvatar.sh
@@ -0,0 +1,9 @@
+curl "https://api.m3o.com/v1/avatar/Generate" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "format": "png",
+  "gender": "female",
+  "upload": true,
+  "username": ""
+}'
