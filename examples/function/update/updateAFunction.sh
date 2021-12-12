52614450aedfcf12d55f1f75efe6e73d03be8a80
m3o-actions <>
2021-12-12 09:32:36 +0000

    Commit from m3o/m3o action

---
+++ examples/function/update/updateAFunction.sh
@@ -0,0 +1,9 @@
+curl "https://api.m3o.com/v1/function/Update" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "entrypoint": "helloworld",
+  "name": "helloworld",
+  "repo": "github.com/m3o/nodejs-function-example",
+  "runtime": "nodejs14"
+}'
