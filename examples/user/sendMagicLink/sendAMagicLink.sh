a4494c7b15e1ee677cc94846c228e0740c069845
m3o-actions <>
2021-12-13 13:51:37 +0000

    Commit from m3o/m3o action

--- examples/user/sendMagicLink/sendAMagicLink.sh
+++ examples/user/sendMagicLink/sendAMagicLink.sh
@@ -1,10 +1,11 @@
-echo '{
+curl "https://api.m3o.com/v1/user/SendMagicLink" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
   "address": "www.example.com",
   "email": "joe@example.com",
   "endpoint": "verifytoken",
   "fromName": "Awesome Dot Com",
   "subject": "MagicLink to access your account",
   "textContent": "Hi there,\n\nClick here to access your account $micro_verification_link"
-}' | \
-websocat -n -H "Authorization: Bearer $M3O_API_TOKEN" \
-wss://api.m3o.com/v1/user/SendMagicLink
+}'
