9695db245ae97795d34dfb472edbc045d91ff85d
m3o-actions <>
2022-02-16 10:05:45 +0000

    Commit from m3o/m3o action

--- examples/user/update/updateAnAccount.sh
+++ examples/user/update/updateAnAccount.sh
@@ -4,5 +4,6 @@ curl "https://api.m3o.com/v1/user/Update" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
   "email": "joe+2@example.com",
-  "id": "user-1"
+  "id": "user-1",
+  "username": "joe"
 }'
