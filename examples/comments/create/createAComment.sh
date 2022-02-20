fb6682c7fa36f7287bfe1bf9998b0824012ef9f7
m3o-actions <>
2022-02-20 12:36:18 +0000

    Commit from m3o/m3o action

---
+++ examples/comments/create/createAComment.sh
@@ -0,0 +1,8 @@
+
+curl "https://api.m3o.com/v1/comments/Create" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "text": "This is my comment",
+  "title": "New Comment"
+}'
---
+++ examples/comments/update/updateAComment.sh
@@ -0,0 +1,11 @@
+
+curl "https://api.m3o.com/v1/comments/Update" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "comment": {
+    "id": "63c0cdf8-2121-11ec-a881-0242e36f037a",
+    "subject": "Update Comment",
+    "text": "Updated comment text"
+  }
+}'
