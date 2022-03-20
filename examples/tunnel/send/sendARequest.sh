178c6c2a112090acfb775e60e096c4ea0323d634
m3o-actions <>
2022-03-20 21:41:56 +0000

    Commit from m3o/m3o action

---
+++ examples/tunnel/send/sendARequest.sh
@@ -0,0 +1,8 @@
+
+curl "https://api.m3o.com/v1/tunnel/Send" \
+-X POST \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "url": "https://google.com"
+}'
