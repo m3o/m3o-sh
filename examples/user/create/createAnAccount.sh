bcaea137075752ce570e1610f4989125a5780b9a
m3o-actions <>
2022-03-25 08:41:35 +0000

    Commit from m3o/m3o action

--- examples/user/create/createAnAccount.sh
+++ examples/user/create/createAnAccount.sh
@@ -1,6 +1,5 @@
 
 curl "https://api.m3o.com/v1/user/Create" \
--X POST \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
--- examples/user/updatePassword/updateTheAccountPassword.sh
+++ examples/user/updatePassword/updateTheAccountPassword.sh
@@ -1,6 +1,5 @@
 
 curl "https://api.m3o.com/v1/user/UpdatePassword" \
--X POST \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
