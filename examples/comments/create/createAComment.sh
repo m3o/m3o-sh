022ee92105c4e9e5aeeef1be49f5c0ad13a5277a
m3o-actions <>
2022-04-06 14:57:34 +0000

    Commit from m3o/m3o action

--- examples/comments/create/createAComment.sh
+++ examples/comments/create/createAComment.sh
@@ -3,6 +3,6 @@ curl "https://api.m3o.com/v1/comments/Create" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
-  "text": "This is my comment",
-  "title": "New Comment"
+  "subject": "New Comment",
+  "text": "This is my comment"
 }'
