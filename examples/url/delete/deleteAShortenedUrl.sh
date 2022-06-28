f9a8ce42f36b6c6b6d93716c626eb200b24b1a3c
m3o-actions <>
2022-06-28 13:02:36 +0000

    Commit from m3o/m3o action

---
+++ examples/url/delete/deleteAShortenedUrl.sh
@@ -0,0 +1,7 @@
+
+curl "https://api.m3o.com/v1/url/Delete" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "shortURL": "https://m3o.one/u/f8f3f83f3f83g"
+}'
