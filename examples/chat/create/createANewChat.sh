913ea43a186b18d49cf86a9104629bf1148b594e
m3o-actions <>
2022-07-20 17:21:17 +0000

    Commit from m3o/m3o action

--- examples/chat/create/createANewChat.sh
+++ examples/chat/create/createANewChat.sh
@@ -3,6 +3,6 @@ curl "https://api.m3o.com/v1/chat/Create" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
-  "description": "The general chat group",
+  "description": "The general group",
   "name": "general"
 }'
