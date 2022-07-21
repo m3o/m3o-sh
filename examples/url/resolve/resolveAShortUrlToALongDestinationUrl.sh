8c823a7ee46413effa88a97613ced806bf153c05
m3o-actions <>
2022-07-21 17:41:54 +0000

    Commit from m3o/m3o action

---
+++ examples/url/resolve/resolveAShortUrlToALongDestinationUrl.sh
@@ -0,0 +1,7 @@
+
+curl "https://api.m3o.com/v1/url/Resolve" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "shortURL": "https://m3o.one/u/ck6SGVkYp"
+}'
