43017987fbb773dbabe7a2170e05bce69d6ba682
m3o-actions <>
2022-07-21 16:37:42 +0000

    Commit from m3o/m3o action

---
+++ examples/url/update/updateAShortUrl.sh
@@ -0,0 +1,8 @@
+
+curl "https://api.m3o.com/v1/url/Update" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "destinationURL": "https://mynewsite.com/this-is-a-rather-long-web-address",
+  "shortURL": "https://m3o.one/u/f8f3f83f3f83g"
+}'
