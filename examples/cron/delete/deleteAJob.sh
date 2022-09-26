b1d5773f8c4baeb90ea66aadeb34419a911765a7
m3o-actions <>
2022-09-26 10:55:16 +0000

    Commit from m3o/m3o action

---
+++ examples/cron/delete/deleteAJob.sh
@@ -0,0 +1,7 @@
+
+curl "https://api.m3o.com/v1/cron/Delete" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $M3O_API_TOKEN" \
+-d '{
+  "id": "0c8cf9f7-3a61-4e91-b249-00a970044c95"
+}'
