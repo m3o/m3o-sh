fb6cc25c9e9cb17e57894c7b6141eb39aff829a8
m3o-actions <>
2023-07-06 14:49:50 +0000

    Commit from m3o/m3o action

--- examples/ai/chat/chatWithChatGpt.sh
+++ examples/ai/chat/chatWithChatGpt.sh
@@ -3,6 +3,16 @@ curl "https://api.m3o.com/v1/ai/Chat" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $M3O_API_TOKEN" \
 -d '{
+  "context": [
+    {
+      "prompt": "who is leonardo davinci",
+      "reply": "Leonardo da Vinci was an Italian polymath during the Renaissance period. He was born in 1452 in Vinci, Italy, and is renowned for his contributions to various fields such as science, engineering, art, and anatomy. Da Vinci's most famous works include the Mona Lisa and The Last Supper. He is often considered one of the greatest artists and thinkers of all time."
+    },
+    {
+      "prompt": "where is he from?",
+      "reply": "Leonardo da Vinci was from Vinci, a town in Tuscany, Italy."
+    }
+  ],
   "model": "gpt-3.5-turbo",
-  "prompt": "who is leonardo davinci"
+  "prompt": "when did he die?"
 }'
